3e8128e28191343df9d5566a8205696d145469a1
Dave <david.geretti@gmail.com>
2020-02-06 23:27:06 +0100

    Finish tuto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useEffect } from 'react';
+
+import API, { graphqlOperation } from '@aws-amplify/api'
+import PubSub from '@aws-amplify/pubsub'
+
+import { createCard } from './graphql/mutations'
+import { listCards } from './graphql/queries'
+import { onCreateCard } from './graphql/subscriptions'
+
+import awsconfig from './aws-exports'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const QUERY = 'QUERY'
+const SUBSCRIPTION = 'SUBSCRIPTION'
+
+const initialState = {
+  cards: []
 }
 
-export default App;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case QUERY:
+      return {...state, cards: action.cards}
+    case SUBSCRIPTION:
+      return {...state, cards: [...state.cards, action.card ]}
+    default:
+      return state
+  }
+}
+
+API.configure(awsconfig)
+PubSub.configure(awsconfig)
+
+async function createNewCard() {
+  const card = { question: 'Coach', answer: 'Trener' }
+  await API.graphql(graphqlOperation(createCard, { input: card }))
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    async function getData() {
+      const cardData = await API.graphql(graphqlOperation(listCards))
+      dispatch({ type: QUERY, cards: cardData.data.listCards.items })
+    }
+    getData()
+
+    const subscription = API.graphql(graphqlOperation(onCreateCard)).subscribe({
+      next: eventData => {
+        const card = eventData.value.data.onCreateCard
+        dispatch({ type: SUBSCRIPTION, card })
+      }
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  return (
+    <div className="App">
+      Hello world
+      <button onClick={createNewCard}>Add new card</button>
+      <div>
+        {state.cards.length > 0 ?
+          state.cards.map(card => <p key={card.id}>{card.question} - {card.answer}</p>) :
+          <p>No card yet!</p>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
